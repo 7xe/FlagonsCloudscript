770c7f247f84cb257b36c0d88a1089891b42dd08
7xe <evans.samuel7@gmail.com>
2018-07-07 15:48:46 +1000

    babys first steps - add vc test

--- Cloudscript.js
+++ Cloudscript.js
@@ -30,6 +30,13 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//--------------------------------------------------------------------------------------------------//
+//Variables
+
+var GOLD_CODE = "GP";
+
+//--------------------------------------------------------------------------------------------------//
+
 // This is a Cloud Script function. "args" is set to the value of the "FunctionParameter" 
 // parameter of the ExecuteCloudScript API.
 // (https://api.playfab.com/Documentation/Client/method/ExecuteCloudScript)
@@ -236,56 +243,28 @@ handlers.unlockHighSkillContent = function (args, context) {
     return { profile: context.playerProfile };
 };
 
-// Photon Webhooks Integration
-//
-// The following functions are examples of Photon Cloud Webhook handlers. 
-// When you enable the Photon Add-on (https://playfab.com/marketplace/photon/)
-// in the Game Manager, your Photon applications are automatically configured
-// to authenticate players using their PlayFab accounts and to fire events that 
-// trigger your Cloud Script Webhook handlers, if defined. 
-// This makes it easier than ever to incorporate multiplayer server logic into your game.
+handlers.BeginDungeon = function (args) {
+    var GetUserInventoryRequest = {
+        "PlayFabId" : currentPlayerId
+    };
+    var GetUserInventoryResult = server.GetUserInventory(GetUserInventoryRequest);
 
+    var userInventory = GetUserInventoryResult.Inventory;
+    var userVCBalances = GetUserInventoryResult.VirtualCurrency;
 
-// Triggered automatically when a Photon room is first created
-handlers.RoomCreated = function (args) {
-    log.debug("Room Created - Game: " + args.GameId + " MaxPlayers: " + args.CreateOptions.MaxPlayers);
-};
+    AddVC(userVCBalances, GOLD_CODE, 50);
+}
 
-// Triggered automatically when a player joins a Photon room
-handlers.RoomJoined = function (args) {
-    log.debug("Room Joined - Game: " + args.GameId + " PlayFabId: " + args.UserId);
-};
-
-// Triggered automatically when a player leaves a Photon room
-handlers.RoomLeft = function (args) {
-    log.debug("Room Left - Game: " + args.GameId + " PlayFabId: " + args.UserId);
-};
-
-// Triggered automatically when a Photon room closes
-// Note: currentPlayerId is undefined in this function
-handlers.RoomClosed = function (args) {
-    log.debug("Room Closed - Game: " + args.GameId);
-};
-
-// Triggered automatically when a Photon room game property is updated.
-// Note: currentPlayerId is undefined in this function
-handlers.RoomPropertyUpdated = function (args) {
-    log.debug("Room Property Updated - Game: " + args.GameId);
-};
-
-// Triggered by calling "OpRaiseEvent" on the Photon client. The "args.Data" property is 
-// set to the value of the "customEventContent" HashTable parameter, so you can use
-// it to pass in arbitrary data.
-handlers.RoomEventRaised = function (args) {
-    var eventData = args.Data;
-    log.debug("Event Raised - Game: " + args.GameId + " Event Type: " + eventData.eventType);
-
-    switch (eventData.eventType) {
-        case "playerMove":
-            processPlayerMove(eventData);
-            break;
-
-        default:
-            break;
+function AddVC(vcBalances, code, amount)
+{
+    if(vcBalances != null && vcBalances.hasOwnProperty(code)){
+        vcBalances[code] += amount;
     }
-};
+
+    var AddUserVirtualCurrencyRequest = {
+        "PlayFabId" : currentPlayerId,
+        "VirtualCurrency" : code,
+        "Amount" : amount
+    };
+    var AddUserVirtualCurrencyResult = server.AddUserVirtualCurrency(AddUserVirtualCurrencyRequest);
+}
